d01c6439f5d9e00a9f0309c63253b35b731252dc
Varunreddy6264 <varunreddy7456@gmail.com>
2025-07-10 18:28:29 +0530

    Add files via upload

---
+++ src/pages/Index.tsx
@@ -0,0 +1,27 @@
+import Navigation from '@/components/Navigation';
+import HeroSection from '@/components/HeroSection';
+import AboutSection from '@/components/AboutSection';
+import SkillsSection from '@/components/SkillsSection';
+import ProjectsSection from '@/components/ProjectsSection';
+import EducationSection from '@/components/EducationSection';
+import TestimonialsSection from '@/components/TestimonialsSection';
+import ContactSection from '@/components/ContactSection';
+import Footer from '@/components/Footer';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navigation />
+      <HeroSection />
+      <AboutSection />
+      <SkillsSection />
+      <ProjectsSection />
+      <EducationSection />
+      <TestimonialsSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
